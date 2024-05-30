e20ea4a11f2a91cd2151fb22022c08a54b036f85
dawid mrotek <DawidMrotek@protonmail.ch>
2024-05-31 01:05:06 +0200

    [CREATE] environment do it yourself

---
+++ reinforcement_learning_sentdex/environment_diy.py
@@ -0,0 +1,118 @@
+import numpy as np
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+import pickle
+from matplotlib import style
+import time
+
+
+style.use("ggplot")
+
+# 10 by 10 grid
+SIZE = 10
+# how many episodes
+HM_EPISODES = 25000
+MOVE_PENALTY = 1
+ENEMY_PEANLTY = 300
+FOOD_REWARD = 25
+
+epsilon = 0.9
+EPS_DECAY = 0.9998
+SHOW_EVERY = 3000
+
+start_q_table = None
+
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+
+PLAYER_N = 1
+FOOD_N = 2
+ENEMY_N = 3
+
+# BGR color
+d = {1: (255, 175, 0), 
+     2: (0, 255, 0),
+     3: (0, 0, 255)}
+
+
+class Blob:
+    def __init__(self):
+        self.x = np.random.randint(0, SIZE)
+        self.y = np.random.randint(0, SIZE)
+    
+    def __str__(self):
+        return f"{self.x}, {self.y}"
+    
+    # overload subtraction operator
+    def __sub__(self, other):
+        return (self.x - other.x, self.y-other.y)
+    
+    def action(self, choice):
+        if choice == 0:
+            self.move(x=1, y=1)
+        elif choice == 1:
+            self.move(x=-1, y=-1)
+        elif choice == 2:
+            self.move(x=-1, y=1)
+        elif choice == 0:
+            self.move(x=1, y=-1)
+
+    def move(self, x=False, y=False):
+        if not x:
+            self.x += np.random.randint(-1, 2)
+        else:
+            self.x += x
+
+        if not y:
+            self.y += np.random.randint(-1, 2)
+        else:
+            self.y += y
+
+        if self.x < 0:
+            self.x = 0
+        elif self.x > SIZE-1:
+            self.x = SIZE-1
+
+        if self.y < 0:
+            self.y = 0
+        elif self.y > SIZE-1:
+            self.y = SIZE-1
+
+
+# create or load q-table
+if start_q_table is None:
+    q_table = {}
+    # observation space -> (x1, y1),(x2,y2), for every combination
+    for x1 in range(-SIZE+1, SIZE):
+        for y1 in range(-SIZE+1, SIZE):
+            for x2 in range(-SIZE+1, SIZE):
+                for y2 in range(-SIZE+1, SIZE):
+                    # add every combination to table
+                    # key in table is tuple of tuples, values initialized as random
+                    q_table[((x1, y1), (x2, y2))] = [np.random.uniform(-5, 0) for i in range(4)]
+else:
+    with open(start_q_table, "rb") as f:
+        q_table = pickle.load(f)
+
+episode_rewards = []
+for episode in range(HM_EPISODES):
+    player = Blob()
+    food = Blob()
+    enemy = Blob()
+
+    if episode % SHOW_EVERY == 0:
+        print(f"on # {episode}, epsilon: {epsilon}")
+        print(f"{SHOW_EVERY} ep mean {np.mean(episode_rewards[-SHOW_EVERY:])}")
+        show = True
+    else:
+        show = False
+
+    episode_reward = 0
+    for i in range(200):
+        obs = (player-food, player-enemy)
+        if np.randomrandom() > epsilon:
+            action = np.argmax(q_table[obs])
+
+
+
