2d9f6ec50b9d73b08e4df6607b02e277f786ae32
dawid mrotek <DawidMrotek@protonmail.ch>
2024-05-31 01:03:13 +0200

    [ADD] show q-tables changes as video

--- reinforcement_learning_sentdex/qlearning.py
+++ reinforcement_learning_sentdex/qlearning.py
@@ -3,11 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 import os
+from datetime import datetime
+import cv2
 
-QTABLES_DIR = 'reinforcement_learning_sentdex/qtables'
+
+QTABLES_DIR = 'qtables'
 if not os.path.exists(QTABLES_DIR):
     os.makedirs(QTABLES_DIR)
 
+QTABLES_IMAGES_DIR = 'qtables_images'
+if not os.path.exists(QTABLES_IMAGES_DIR):
+    os.makedirs(QTABLES_IMAGES_DIR)
+
+
+def showTime():
+    return str("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')+" UTC]")
 
 
 def train(episodes=25000, show_every=100):
@@ -133,14 +143,59 @@ def show_qtable_actions(episodes=25000, show_every=100):
     plt.show()
 
 
+# good luck running that in any resonable time
+def qtables_to_images(episodes=25000, show_every=100, overwrite=True):
+
+    style.use('ggplot')
+
+    fig = plt.figure(figsize=(12, 9))
+
+    for i in range(0, episodes, show_every):
+        print(F"{showTime()} Loop: {i}")
+        if overwrite == False and os.path.isfile(f"{QTABLES_IMAGES_DIR}/{i}.png"):
+            pass
+        else:
+            ax1 = fig.add_subplot(311)
+            ax2 = fig.add_subplot(312)
+            ax3 = fig.add_subplot(313)
+            q_table = np.load(f"{QTABLES_DIR}/{i}-qtable.npy")
+
+            for x, x_vals in enumerate(q_table):
+                for y, y_vals in enumerate(x_vals):
+                    ax1.scatter(x, y, c=get_q_color(y_vals[0], y_vals)[0], marker="o", alpha=get_q_color(y_vals[0], y_vals)[1])
+                    ax2.scatter(x, y, c=get_q_color(y_vals[1], y_vals)[0], marker="o", alpha=get_q_color(y_vals[1], y_vals)[1])
+                    ax3.scatter(x, y, c=get_q_color(y_vals[2], y_vals)[0], marker="o", alpha=get_q_color(y_vals[2], y_vals)[1])
+
+                    ax1.set_ylabel("Action 0")
+                    ax2.set_ylabel("Action 1")
+                    ax3.set_ylabel("Action 2")
+
+            #plt.show()
+            plt.savefig(f"{QTABLES_IMAGES_DIR}/{i}.png")
+            plt.clf()
 
 
+def make_video(episodes=25000, show_every=100):
+    # windows:
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # Linux:
+    #fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+    out = cv2.VideoWriter('qlearn.avi', fourcc, 60.0, (1200, 900))
+
+    for i in range(0, episodes, show_every):
+        img_path = f"{QTABLES_IMAGES_DIR}/{i}.png"
+        print(img_path)
+        frame = cv2.imread(img_path)
+        out.write(frame)
+
+    out.release()
 
 
 if __name__ == "__main__":
     episodes=30000
     show_every=10
 
-    train(episodes=episodes, show_every=show_every)
-    show_qtable_actions(episodes=episodes, show_every=show_every)
-
+    #train(episodes=episodes, show_every=show_every)
+    #show_qtable_actions(episodes=episodes, show_every=show_every)
+    #qtables_to_images(episodes=episodes, show_every=show_every, overwrite=False)
+    #make_video(episodes=episodes, show_every=show_every)
